0aee32f679e4ca1fa7e82be1f3b3789248020f4e
lirik <lirikrexhepiii@gmail.com>
2024-06-01 00:58:32 +0200

    Created all the Content Files

--- dashboard/components/sidebar.php
+++ dashboard/components/sidebar.php
@@ -1,54 +1,51 @@
 <div class="flex flex-col bg-slate-100 w-80 h-screen border-r border-slate-300 gap-6 pt-14">
-    <!--Dashboard-->
-    <span class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
+    <!-- Dashboard -->
+    <a href="?dashboard" class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
         <span class="material-symbols-outlined">
             space_dashboard
         </span>
         <span class="text-lg font-semibold">
             Dashboard
         </span>
-    </span>
+    </a>
 
-    <!--Users-->
-    <span class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
+    <!-- Users -->
+    <a href="?user-management" class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
         <span class="material-symbols-outlined">
             group
         </span>
         <span class="text-lg font-semibold">
             User Management
         </span>
-    </span>
+    </a>
 
-     <!--Clients-->
-     <span class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
+    <!-- Clients -->
+    <a href="?client-management" class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
         <span class="material-symbols-outlined">
             supervisor_account
         </span>
         <span class="text-lg font-semibold">
             Client Management
         </span>
-    </span>
+    </a>
     
-    <!--Dashboard-->
-    <span class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
+    <!-- Departments -->
+    <a href="?departments" class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
         <span class="material-symbols-outlined">
             linked_services
         </span>
         <span class="text-lg font-semibold">
-            Departaments
+            Departments
         </span>
-    </span>
+    </a>
     
-    <!--Dashboard-->
-    <span class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
+    <!-- Projects -->
+    <a href="?projects" class="flex flex-row items-center gap-2 py-2 px-4 mx-4 rounded hover:bg-blue-100">
         <span class="material-symbols-outlined">
             tactic
         </span>
         <span class="text-lg font-semibold">
             Projects
         </span>
-    </span>    
-    
-
-
+    </a>
 </div>
